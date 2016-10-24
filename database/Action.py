62c3f28e05d4f5c7610161235fd40291e4906f0d
xushuainan <xsnshuai@163.com>
2016-10-24 17:09:44 +0800

    职业导航第一版

--- database/Action.py
+++ database/Action.py
@@ -2492,8 +2492,8 @@ class Action(object):
 
         if cache_flag:
             # 读取缓存
-            query_cache_data = None
-            # query_cache_data = self.cacheredis.get('salary_tantile_list_{search}'.format(search=search.upper()))
+            # query_cache_data = None
+            query_cache_data = self.cacheredis.get('salary_tantile_list_{search}'.format(search=search.upper()))
 
             # 读取缓存失败
             if query_cache_data is None:
